54bea204dc473654a3d35c24d8471ced70a63280
Travis Arnold <ftntravis@gmail.com>
2015-08-29 21:42:01 -0700

    ran build

--- dist/react-dately.js
+++ dist/react-dately.js
@@ -66,11 +66,11 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _Calendar2 = _interopRequireDefault(_Calendar);
 
-	var _Time = __webpack_require__(6);
+	var _Time = __webpack_require__(7);
 
 	var _Time2 = _interopRequireDefault(_Time);
 
-	var _utils = __webpack_require__(5);
+	var _utils = __webpack_require__(6);
 
 	var _utils2 = _interopRequireDefault(_utils);
 
@@ -89,6 +89,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	});
 
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+	var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
@@ -112,16 +114,37 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _NextMonth2 = _interopRequireDefault(_NextMonth);
 
-	var _utils = __webpack_require__(5);
+	var _extend = __webpack_require__(5);
+
+	var _extend2 = _interopRequireDefault(_extend);
+
+	var _utils = __webpack_require__(6);
 
 	var WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 	var ENTER_KEY = 13;
 
 	var Day = (function (_Component) {
 	  function Day() {
+	    var _this = this;
+
 	    _classCallCheck(this, Day);
 
 	    _get(Object.getPrototypeOf(Day.prototype), 'constructor', this).apply(this, arguments);
+
+	    this.rules = {
+	      today: function today(date) {
+	        return (0, _utils.isSame)(date, new Date());
+	      },
+	      outside: function outside(date, month) {
+	        return (0, _utils.isOutsideMonth)(date, month);
+	      },
+	      before: function before(date) {
+	        return _this.props.minDay && [(0, _utils.isBeforeDay)(date, _this.props.minDay), true];
+	      },
+	      after: function after(date) {
+	        return _this.props.maxDay && [(0, _utils.isAfterDay)(date, _this.props.maxDay), true];
+	      }
+	    };
 	  }
 
 	  _inherits(Day, _Component);
@@ -140,8 +163,8 @@ return /******/ (function(modules) { // webpackBootstrap
 	    }
 	  }, {
 	    key: '_renderDay',
-	    value: function _renderDay(day) {
-	      return this.props.renderDay(day);
+	    value: function _renderDay(day, rules) {
+	      return this.props.renderDay(day, rules);
 	    }
 	  }, {
 	    key: 'render',
@@ -149,55 +172,52 @@ return /******/ (function(modules) { // webpackBootstrap
 	      var _props = this.props;
 	      var month = _props.month;
 	      var date = _props.date;
-	      var disabledDays = _props.disabledDays;
 	      var minDay = _props.minDay;
 	      var maxDay = _props.maxDay;
-	      var selectedDays = _props.selectedDays;
 	      var outsideDays = _props.outsideDays;
 	      var onClick = _props.onClick;
 	      var canTouchTap = _props.canTouchTap;
+	      var onMouseDown = _props.onMouseDown;
+	      var onMouseMove = _props.onMouseMove;
+	      var onMouseUp = _props.onMouseUp;
 
+	      var rules = (0, _extend2['default'])(this.rules, this.props.rules);
+	      var isOutside = (0, _utils.isOutsideMonth)(date, month);
 	      var className = 'cal__day';
 	      var modifiers = [];
 	      var onDayClick = this._handleDateSelect.bind(this, date);
+	      var onDayMouseDown = onMouseDown.bind(this, date);
+	      var onDayMouseMove = onMouseMove.bind(this, date);
+	      var onDayMouseUp = onMouseUp.bind(this, date);
+	      var isDisabled = undefined;
 
-	      var isToday = (0, _utils.isSame)(date, new Date());
+	      Object.keys(rules).forEach(function (key) {
+	        var result = rules[key](date, month);
+	        isDisabled = false;
 
-	      if (isToday) {
-	        modifiers.push('today');
-	      }
+	        // if result is an array, deconstruct it
+	        if (result && result.constructor === Array) {
+	          var _result = _slicedToArray(result, 2);
 
-	      var isOutside = (0, _utils.isOutsideMonth)(date, month);
-
-	      if (isOutside) {
-	        modifiers.push('outside');
-	      }
-
-	      var isDisabled = disabledDays ? (0, _utils.isSame)(date, disabledDays) : null;
-	      var isBefore = minDay ? (0, _utils.isBeforeDay)(date, minDay) : null;
-	      var isAfter = maxDay ? (0, _utils.isAfterDay)(date, maxDay) : null;
-
-	      if (isDisabled || isBefore || isAfter) {
-	        modifiers.push('disabled');
-	        onDayClick = null;
-	      }
-
-	      if (selectedDays) {
-
-	        var isSelected = (0, _utils.isSame)(date, selectedDays);
-
-	        if (isSelected || isSelected === 0) {
-	          modifiers.push('selected');
+	          result = _result[0];
+	          isDisabled = _result[1];
 	        }
 
-	        if (isSelected === 0) {
-	          modifiers.push('selected-first');
-	        }
+	        if (result) {
+	          // camelCase to hyphen friendly class name
+	          var modifier = key.replace(/[a-z][A-Z]/g, function (str, offset) {
+	            return str[0] + '-' + str[1].toLowerCase();
+	          });
+	          modifiers.push(modifier);
 
-	        if (isSelected === selectedDays.length - 1) {
-	          modifiers.push('selected-last');
+	          // make sure events get disabled as well
+	          if (isDisabled) {
+	            onDayClick = onDayMouseDown = onDayMouseMove = onDayMouseUp = function (e) {
+	              return e.preventDefault();
+	            };
+	          }
 	        }
-	      }
+	      });
 
 	      className += modifiers.map(function (modifier) {
 	        return ' ' + className + '--' + modifier;
@@ -213,12 +233,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	          role: 'presentation',
 	          'aria-label': date,
 	          className: className,
+	          tabIndex: isDisabled ? null : 0,
 	          onClick: canTouchTap ? null : onDayClick,
 	          onTouchTap: canTouchTap ? onDayClick : null,
-	          tabIndex: isDisabled || isBefore || isAfter ? null : 0,
-	          onKeyDown: this._handleKeyDown.bind(this, date)
+	          onKeyDown: this._handleKeyDown.bind(this, date),
+	          onMouseDown: onDayMouseDown,
+	          onMouseMove: onDayMouseMove,
+	          onMouseUp: onDayMouseUp
 	        },
-	        this._renderDay(date)
+	        this._renderDay(date, rules)
 	      );
 	    }
 	  }]);
@@ -238,10 +261,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	  _createClass(Week, [{
 	    key: 'render',
 	    value: function render() {
-	      var _this = this;
+	      var _this2 = this;
 
 	      var days = this.props.days.map(function (day) {
-	        return _react2['default'].createElement(Day, _extends({}, _this.props, {
+	        return _react2['default'].createElement(Day, _extends({}, _this2.props, {
 	          key: day.getTime(),
 	          date: day
 	        }));
@@ -267,29 +290,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	    this.state = {
 	      month: this.props.date
 	    };
+	    this._id = this._generateId();
 	  }
 
 	  _inherits(Calendar, _Component3);
 
 	  _createClass(Calendar, [{
-	    key: 'componentWillMount',
-	    value: function componentWillMount() {
-	      if (this.props.selectedDays) {
-	        this._normalizeDates(this.props.selectedDays);
-	      }
-	      if (this.props.disabledDays) {
-	        this._normalizeDates(this.props.disabledDays);
-	      }
-	    }
-	  }, {
 	    key: 'componentWillReceiveProps',
 	    value: function componentWillReceiveProps(nextProps) {
-	      if (nextProps.selectedDays) {
-	        this._normalizeDates(nextProps.selectedDays);
-	      }
-	      if (nextProps.disabledDays) {
-	        this._normalizeDates(nextProps.disabledDays);
-	      }
 	      if (this.props.month !== nextProps.month) {
 	        this.setState({ month: nextProps.date });
 	      }
@@ -303,59 +311,41 @@ return /******/ (function(modules) { // webpackBootstrap
 	    key: 'navigateMonth',
 	    value: function navigateMonth(direction) {
 	      var month = this.state.month;
+
 	      this.setState({ month: (0, _utils.navigateMonth)(month, direction) });
 	    }
 	  }, {
-	    key: '_normalizeDates',
-	    value: function _normalizeDates(mixed) {
+	    key: '_generateId',
 
-	      var month = new Date();
+	    // used for ARIA support
+	    value: function _generateId() {
+	      var timestamp = Date.now();
+	      var uniqueNumber = 0;
 
-	      for (var i = mixed.length; i--;) {
-
-	        var mix = mixed[i];
-
-	        // if it's a Date object already then push it
-	        // and contiue
-	        if (mix instanceof Date) {
-	          mixed[i] = mix;
-	          continue;
+	      (function () {
+	        // If created at same millisecond as previous
+	        if (timestamp <= uniqueNumber) {
+	          timestamp = ++uniqueNumber;
+	        } else {
+	          uniqueNumber = timestamp;
 	        }
+	      })();
 
-	        // test if digit and in between current month
-	        // or test to block day of week out somehow
-	        // reference pickadate and how they do it
-	        // just block out day for now
-	        if (/^\d+$/.test(mix)) {
-	          mixed[i] = new Date(month.getFullYear(), month.getMonth(), mix);
-	          continue;
-	        }
-
-	        if (Array.isArray(mix)) {
-	          mixed[i] = new Date(mix[0], mix[1], mix[2]);
-	          continue;
-	        }
-	      }
-
-	      // finally sort the dates so they're in order
-	      mixed.sort(function (a, b) {
-	        a = a.getTime();
-	        b = b.getTime();
-	        return a < b ? -1 : a > b ? 1 : 0;
-	      });
+	      return 'D' + timestamp;
 	    }
 	  }, {
 	    key: '_renderWeekdays',
 	    value: function _renderWeekdays() {
-	      var _this2 = this;
+	      var _props2 = this.props;
+	      var trimWeekdays = _props2.trimWeekdays;
+	      var weekStartsOn = _props2.weekStartsOn;
 
 	      var weekdays = WEEKDAYS.slice(0);
-	      var sortedWeekdays = weekdays.concat(weekdays.splice(0, this.props.weekStartsOn));
+	      var sortedWeekdays = weekdays.concat(weekdays.splice(0, weekStartsOn));
 
 	      var getDays = function getDays() {
 	        return sortedWeekdays.map(function (weekday, index) {
-	          var trim = _this2.props.trimWeekdays;
-	          var weekdayTrimmed = trim !== null ? weekday.substring(0, parseInt(trim)) : weekday;
+	          var weekdayTrimmed = trimWeekdays ? weekday.substring(0, parseInt(trimWeekdays)) : weekday;
 	          return _react2['default'].createElement(
 	            'th',
 	            {
@@ -385,9 +375,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	      var _this3 = this;
 
 	      var month = this.state.month;
-	      var _props2 = this.props;
-	      var weekStartsOn = _props2.weekStartsOn;
-	      var forceSixRows = _props2.forceSixRows;
+	      var _props3 = this.props;
+	      var weekStartsOn = _props3.weekStartsOn;
+	      var forceSixRows = _props3.forceSixRows;
 
 	      var weeks = (0, _utils.getWeeks)(month, weekStartsOn, forceSixRows).map(function (week, index) {
 	        return _react2['default'].createElement(Week, _extends({}, _this3.props, {
@@ -404,34 +394,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	      );
 	    }
 	  }, {
-	    key: 'generateID',
-	    value: function generateID() {
-
-	      var timestamp = Date.now(),
-	          uniqueNumber = 0;
-
-	      (function () {
-	        // If created at same millisecond as previous
-	        if (timestamp <= uniqueNumber) {
-	          timestamp = ++uniqueNumber;
-	        } else {
-	          uniqueNumber = timestamp;
-	        }
-	      })();
-
-	      return 'D' + timestamp;
-	    }
-	  }, {
 	    key: 'render',
 	    value: function render() {
-	      var _props3 = this.props;
-	      var className = _props3.className;
-	      var minDay = _props3.minDay;
-	      var maxDay = _props3.maxDay;
-	      var canTouchTap = _props3.canTouchTap;
+	      var _props4 = this.props;
+	      var className = _props4.className;
+	      var minDay = _props4.minDay;
+	      var maxDay = _props4.maxDay;
+	      var canTouchTap = _props4.canTouchTap;
 	      var month = this.state.month;
-
-	      var ID = this.generateID();
 
 	      var classes = [];
 	      var modifiers = this.props.modifiers ? this.props.modifiers.split(',') : null;
@@ -474,7 +444,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	            onTouchTap: canTouchTap ? this.navigateMonth.bind(this, -1) : null,
 	            inner: this.props.prevHTML,
 	            disable: prevDisabled,
-	            controls: ID + '_table'
+	            controls: this._id + '_table'
 	          }),
 	          _react2['default'].createElement(
 	            'div',
@@ -495,12 +465,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	            onTouchTap: canTouchTap ? this.navigateMonth.bind(this, 1) : null,
 	            inner: this.props.nextHTML,
 	            disable: nextDisabled,
-	            controls: ID + '_table'
+	            controls: this._id + '_table'
 	          })
 	        ),
 	        _react2['default'].createElement(
 	          'table',
-	          { id: ID + '_table', className: 'cal__table' },
+	          { id: this._id + '_table', className: 'cal__table' },
 	          this._renderWeekdays(),
 	          this._renderWeeksInMonth()
 	        )
@@ -512,8 +482,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	      date: _react.PropTypes.instanceOf(Date),
 	      minDay: _react.PropTypes.instanceOf(Date),
 	      maxDay: _react.PropTypes.instanceOf(Date),
-	      disabledDays: _react.PropTypes.array,
-	      selectedDays: _react.PropTypes.array,
 	      trimWeekdays: _react.PropTypes.number,
 	      weekStartsOn: _react.PropTypes.number,
 	      forceSixRows: _react.PropTypes.bool,
@@ -523,6 +491,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	      prevDisabled: _react.PropTypes.bool,
 	      nextDisabled: _react.PropTypes.bool,
 	      onDateSelect: _react.PropTypes.func,
+	      onMouseDown: _react.PropTypes.func,
+	      onMouseMove: _react.PropTypes.func,
+	      onMouseUp: _react.PropTypes.func,
 	      renderDay: _react.PropTypes.func
 	    },
 	    enumerable: true
@@ -532,8 +503,6 @@ return /******/ (function(modules) { // webpackBootstrap
 	      date: new Date(),
 	      minDay: null,
 	      maxDay: null,
-	      disabledDays: null,
-	      selectedDays: null,
 	      trimWeekdays: null,
 	      weekStartsOn: 0,
 	      forceSixRows: true,
@@ -541,8 +510,18 @@ return /******/ (function(modules) { // webpackBootstrap
 	      prevHTML: '',
 	      nextHTML: '',
 	      canTouchTap: false,
+	      rules: {},
 	      //locale: 'en',
 	      onDateSelect: function onDateSelect() {
+	        return null;
+	      },
+	      onMouseDown: function onMouseDown() {
+	        return null;
+	      },
+	      onMouseMove: function onMouseMove() {
+	        return null;
+	      },
+	      onMouseUp: function onMouseUp() {
 	        return null;
 	      },
 	      renderDay: function renderDay(day) {
@@ -557,6 +536,10 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	exports['default'] = Calendar;
 	module.exports = exports['default'];
+
+	// Rules are how the particular day should behave,
+	// it will add a modifier class to allow styling
+	// one day we could provide a hook for inline styles
 
 /***/ },
 /* 2 */
@@ -746,6 +729,32 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 5 */
 /***/ function(module, exports) {
 
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	exports["default"] = function (out) {
+	  out = out || {};
+
+	  for (var i = 1; i < arguments.length; i++) {
+	    if (!arguments[i]) continue;
+
+	    for (var key in arguments[i]) {
+	      if (arguments[i].hasOwnProperty(key)) out[key] = arguments[i][key];
+	    }
+	  }
+
+	  return out;
+	};
+
+	module.exports = exports["default"];
+
+/***/ },
+/* 6 */
+/***/ function(module, exports) {
+
 	'use strict';
 
 	Object.defineProperty(exports, '__esModule', {
@@ -873,9 +882,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	      return d1.getDate() === d2.getDate() && is.month(d1, d2);
 	    };
 
-	    if (Array.isArray(d2)) {
+	    if (!d1 || !d2) return false;
+
+	    if (d2.constructor === Array) {
 	      for (var i = d2.length; i--;) {
-	        if (is[type](d1, d2[i])) return i;
+	        if (is[type](d1, d2[i])) return true;
 	      }
 	      return false;
 	    } else {
@@ -900,10 +911,17 @@ return /******/ (function(modules) { // webpackBootstrap
 	  },
 
 	  getDaysBetween: function getDaysBetween(startDate, endDate) {
+	    // swap values if start date is after end date
+	    if (utils.isAfterDay(startDate, endDate)) {
+	      var _ref = [endDate, startDate];
+	      startDate = _ref[0];
+	      endDate = _ref[1];
+	    }
 
 	    var days = [startDate];
 	    var current = startDate;
 
+	    // if days are the same bail
 	    if (utils.isSame(startDate, endDate)) {
 	      return days;
 	    }
@@ -929,7 +947,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = exports['default'];
 
 /***/ },
-/* 6 */
+/* 7 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
