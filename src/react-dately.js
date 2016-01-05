37352fbd12d0fa76719505ea84256ed3fdbe499b
Travis Arnold <ftntravis@gmail.com>
2016-01-05 14:16:40 -0800

    give credz

--- src/react-dately.js
+++ src/react-dately.js
@@ -1,3 +1,7 @@
+// Thanks to the following libraries for the inspiration
+// https://github.com/gpbl/react-day-picker
+// https://github.com/amsul/pickadate.js
+
 export { default as Calendar } from './Calendar'
 export { default as Time } from './Time'
 export { default as utils } from './utils'
