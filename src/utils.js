390373deac98cef048a13931b7d964155821b2b1
Robert Mosolgo <rdmosolgo@gmail.com>
2016-08-30 14:39:33 -0400

    doc(utils) add docs to utils functions

--- src/utils.js
+++ src/utils.js
@@ -2,23 +2,30 @@ const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 const DAYS_IN_WEEK = 7
 
 const utils = {
+  // Make a new Date pointing to the same time as `d`
   clone(d) {
     return new Date(d.getTime())
   },
 
+  // Make a new date pointing to the first day of `d`'s month
   getFirstDayOfMonth(d) {
     return new Date(d.getFullYear(), d.getMonth(), 1)
   },
 
+  // Return the number of days in `d`'s month
   getDaysInMonth(d) {
     const resultDate = utils.getFirstDayOfMonth(d)
 
+    // Bump it to the next month, then remove a day
+    // so that it's on the last day of its original month
     resultDate.setMonth(resultDate.getMonth() + 1)
     resultDate.setDate(resultDate.getDate() - 1)
 
     return resultDate.getDate()
   },
 
+  // Return an array of Dates, with one date
+  // for each day in `d`'s month
   getDays(d) {
     let daysInMonth = utils.getDaysInMonth(d)
     let days = []
@@ -31,6 +38,11 @@ const utils = {
     return days
   },
 
+  // Get a two-dimensional array of Dates for `d`'s month.
+  // Each inner array is one week.
+  // Extra days will be added from other months to make each week complete.
+  // Pass `0-7` as `firstDayOfWeek` to specify when weeks should begin (values correspond to `Date.getDay()`).
+  // Pass `true` to `forceSixRows` to make sure each month gets six rows (this is for UI consistency).
   getWeeks(d, firstDayOfWeek = 0, forceSixRows = false) {
     let days = utils.getDays(d)
     let daysInMonth = days.length
@@ -99,18 +111,24 @@ const utils = {
     return weeks
   },
 
+  // Return a new date which is `direction` months away from `d`
+  // (negative `direction` returns a date towards the past)
   navigateMonth(d, direction) {
     const currMonth = utils.clone(d)
     const newMonth = currMonth.setMonth(d.getMonth() + direction, 1)
     return new Date(newMonth)
   },
 
+  // Return a new date which is `7 * direction` days away from `d`
+  // (negative `direction` returns a date towards the past)
   navigateWeek(d, direction) {
     const currWeek = utils.clone(d)
     const newWeek = currWeek.setDate(d.getDate() + (7 * direction))
     return new Date(newWeek)
   },
 
+  // Compare `d1` & `d2` with varying degrees of specificity: `"day"`, `"month"` or `"year"`
+  // If `d2` is an array of dates, return true if _any_ dates match `d1`
   isSame(d1, d2, type = 'day') {
     const is = {}
 
@@ -140,22 +158,27 @@ const utils = {
     }
   },
 
+  // `true` if `d1` is before the beginning of `d2`'s day
   isBeforeDay(d1, d2) {
     d2 = utils.clone(d2)
     d2.setHours(0, 0, 0, 0)
     return d1 < d2
   },
 
+  // `true` if `d1` is after the beginning of `d2`'s day
   isAfterDay(d1, d2) {
     d2 = utils.clone(d2)
     d2.setHours(0, 0, 0, 0)
     return d1 > d2
   },
 
+  // `true` if d1 is in a different month than d2
   isOutsideMonth(d1, d2) {
     return d1.getMonth() !== d2.getMonth()
   },
 
+  // Return an array of dates between `startDate` and `endDate`
+  // The array will be ascending
   getDaysBetween(startDate, endDate) {
     // swap values if start date is after end date
     if (utils.isAfterDay(startDate, endDate)) {
@@ -178,18 +201,23 @@ const utils = {
     return days
   },
 
+  // zero-indexed array of month names
+  // eg, `getMonths()[0] // "January"`
   getMonths() {
     return MONTHS
   },
 
+  // Get the month name for `d`'s month
   formatMonth(d) {
-    return `${MONTHS[d.getMonth()]}`
+    return `MONTHS[d.getMonth()]}`
   },
 
+  // Get 4-digit year from `d`
   formatYear(d) {
     return d.getFullYear()
   },
 
+  // Make a new date comprised of day(date) + minutes
   addMinutesToDay(date, minutes) {
     // default to today's date
     date = date || new Date()
@@ -204,6 +232,10 @@ const utils = {
 
   },
 
+  // Returns hyphenized keys from `rules` where its tests return true
+  // - `rules`: modifier-test pairs where:
+  //   - `test` will be called with `test(day, month)` for each calendar day
+  //   - `modifier` will be hyphenized into a class name if `test(day, month)` returns true
   getModifiers(rules, day, month) {
     const modifiers = []
 
